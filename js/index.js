365afec8a0481bbdc25aa92ab7cb6be44ea2ae85
Nadia Lutz <na.lutz@outlook.com>
2025-02-04 09:25:59 +0100

    services HTML appended

--- js/index.js
+++ js/index.js
@@ -29,3 +29,33 @@ newHeroSection.appendChild(heroIcon);
 
 heroSection.appendChild(newHeroSection);
 
+
+
+// ---------------- this services ------------------ // 
+
+
+let servicesSection = document.querySelector(".services")
+
+services.forEach(services => {
+  let newService = document.createElement("article")
+  newService.classList.add("service");
+
+    let newIllustration = document.createElement("img")
+    newIllustration.src = services.illustration;
+    newService.append(newIllustration)
+
+    let serviceHeadline = document.createElement("h3")
+    serviceHeadline.textContent = services.headline;
+    newService.append(serviceHeadline)
+
+    let serviceText = document.createElement("p")
+    serviceText.textContent = services.text;
+    newService.append(serviceText)
+
+    let serviceLink = document.createElement("a")
+    serviceLink.textContent = services.linktext;
+    newService.append(serviceLink)
+
+    servicesSection.append(newService);
+ });
+
