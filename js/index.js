77073091f1e3014b3390252e699bbc83387ef520
Nadia Lutz <na.lutz@outlook.com>
2025-02-05 10:19:46 +0100

    added about a million stylesheets and trying to make it all work

--- js/index.js
+++ js/index.js
@@ -1,31 +1,38 @@
 // slå dig løs her... 
 
-//eksempel på at udskrive alle overskrifter i services i konsollen:
+// eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
-
 
 // ---------------- this is the hero ------------------ // 
 
 let heroSection = document.querySelector(".hero");
 let newHeroSection = document.createElement("section");
+let heroDiv = document.createElement("div");
 
 let heroHeadline = document.createElement("h2");
 heroHeadline.textContent = hero.headline;
-newHeroSection.appendChild(heroHeadline);
 
+let heroBtn = document.createElement("button");
+heroBtn.textContent = hero.button;
 
 let heroCopy = document.createElement("p");
 heroCopy.textContent = hero.copy;
-newHeroSection.appendChild(heroCopy);
+
+// let heroIcon = document.createElement("img");
+// heroIcon.src = hero.icon;
+
+
+// heroDiv.appendChild(heroIcon);
+heroDiv.appendChild(heroHeadline);
+heroDiv.appendChild(heroCopy);
+heroDiv.appendChild(heroBtn)
+newHeroSection.appendChild(heroDiv);
 
 
 let heroImage = document.createElement("img");
 heroImage.src = hero.image;
 newHeroSection.appendChild(heroImage);
 
-let heroIcon = document.createElement("img");
-heroIcon.src = hero.icon;
-newHeroSection.appendChild(heroIcon);
 
 heroSection.appendChild(newHeroSection);
 
@@ -33,40 +40,39 @@ heroSection.appendChild(newHeroSection);
 
 // ---------------- this is services ------------------ // 
 
-
-let servicesSection = document.querySelector(".services")
+let servicesSection = document.querySelector(".services");
 
 services.forEach(services => {
-  let newService = document.createElement("article")
+  let newService = document.createElement("article");
   newService.classList.add("service");
 
-    let newIllustration = document.createElement("img")
-    newIllustration.src = services.illustration;
-    newService.append(newIllustration)
+  let newIllustration = document.createElement("img");
+  newIllustration.src = services.illustration;
+  newIllustration.classList.add("service__img")
+  newService.append(newIllustration);
 
-    let serviceHeadline = document.createElement("h3")
-    serviceHeadline.textContent = services.headline;
-    newService.append(serviceHeadline)
+  let serviceHeadline = document.createElement("h3");
+  serviceHeadline.textContent = services.headline;
+  newService.append(serviceHeadline);
 
-    let serviceText = document.createElement("p")
-    serviceText.textContent = services.text;
-    newService.append(serviceText)
+  let serviceText = document.createElement("p");
+  serviceText.textContent = services.text;
+  newService.append(serviceText);
 
-    let serviceLink = document.createElement("a")
-    serviceLink.textContent = services.linktext;
-    newService.append(serviceLink)
+  let serviceLink = document.createElement("a");
+  serviceLink.textContent = services.linktext;
+  newService.append(serviceLink);
 
-    servicesSection.append(newService);
- });
+  servicesSection.append(newService);
+});
 
 // ---------------- this is facilities ------------------ // 
 
-
 let facilitySection = document.querySelector(".facilities");
 
-let facilityHeadline = document.createElement("h2")
+let facilityHeadline = document.createElement("h2");
 facilityHeadline.textContent = facilities.headline;
-facilitySection.append(facilityHeadline) 
+facilitySection.append(facilityHeadline);
 
 facilities.options.forEach(option => {
     let newFacility = document.createElement("section"); 
@@ -75,15 +81,12 @@ facilities.options.forEach(option => {
         <img src="${option.icon}">
         <h3>${option.headline}</h3>
         <p>${option.text}</p>  
-        `; 
+    `; 
 
     facilitySection.append(newFacility); 
 });
 
-
-
 // ---------------- this is sites ------------------ // 
-
 
 let sitesSection = document.querySelector(".sites");
 
@@ -93,17 +96,13 @@ sitesHeadline.textContent = sites.headline;
 let sitesText = document.createElement("p");
 sitesText.textContent = sites.text;
 
-
 let sitesLink = document.createElement("button");
 sitesLink.innerHTML = `
 <img src="${sites.btnicon}">
 <p>Start</p>
-`
+`;
 
 sitesSection.append(sitesHeadline, sitesText, sitesLink);
-
-
-
 
 sites.places.forEach(place => {
     let newSites = document.createElement("section");
@@ -118,15 +117,12 @@ sites.places.forEach(place => {
     sitesSection.append(newSites);
 });
 
-
 // ---------------- this is advantages ------------------ // 
-
 
 let advantagesSection = document.querySelector(".advantages");
 
-let advantagesHeadline = document.createElement("h1")
- advantagesHeadline.textContent = ("Our Advantages")
-
+let advantagesHeadline = document.createElement("h1");
+advantagesHeadline.textContent = ("Our Advantages");
 
 advantages.forEach(advantage => {
     let newAdvantages = document.createElement("section");
@@ -134,7 +130,7 @@ advantages.forEach(advantage => {
     newAdvantages.innerHTML = `
         <img src="${advantage.icon}">
         <h2>${advantage.headline}</h2>
-         <p>${advantage.text}</p> 
+        <p>${advantage.text}</p> 
     `;
 
     advantagesSection.append(newAdvantages, advantagesHeadline);
@@ -164,8 +160,10 @@ footer.info.forEach(foot => {
         let li = document.createElement("li");
         li.textContent = item; 
         ul.appendChild(li);
-
     });
     
     footerSection.append(newFooter, footerHeadline, ul); 
 });
+    
+    footerSection.append(newFooter, footerHeadline, ul); 
+
