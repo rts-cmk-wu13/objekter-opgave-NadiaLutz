78e6340c4a533806aeb5cf12204b51fce7972044
Nadia Lutz <na.lutz@outlook.com>
2025-02-05 10:51:07 +0100

    added some changes to JS and created a div to contain some content. also set up some grid displays and added styling to the services section.

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ let hero = {
     headline: "Find out how we can save your time in Backpacking in Europe",
     copy: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac amet id eget scelerisque amet etiam in sit commodo. Pretium ut duis orci pulvinar pretium id consectetur.",
     icon: "https://dazzling-squirrel-77a63d.netlify.app/imgs/icons/Globe.png",
-    button: "explore"
+    button: "Explore"
 }
 
 
